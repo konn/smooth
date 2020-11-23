ab3020e6ce6dac36f3a042b53e4e446f7a2572d5
Hiromi ISHII <konn.jinro@gmail.com>
2020-11-23 16:27:35 +0900

    Restructures exports

--- src/Numeric/Algebra/Smooth.hs
+++ src/Numeric/Algebra/Smooth.hs
@@ -3,22 +3,19 @@
 {-# LANGUAGE ScopedTypeVariables, TypeOperators                          #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Presburger #-}
-module Numeric.Algebra.Smooth where
-import qualified AlgebraicPrelude                   as AP
-import           Control.Lens
-import           Data.Singletons.Prelude            hiding (type (+), type (-))
-import           Data.Sized.Builtin                 (Sized)
-import qualified Data.Sized.Builtin                 as SV
-import           Data.Type.Natural.Builtin
-import           Data.Type.Natural.Class.Arithmetic
-import           Data.Vector                        (Vector)
-import           GHC.TypeNats
-import           Numeric.Algebra                    (Additive, Algebra, (.*))
-import qualified Numeric.Algebra                    as NA
+module Numeric.Algebra.Smooth
+  ( diff1,
+    module Numeric.Algebra.Smooth.Classes,
+    module Numeric.Algebra.Smooth.Dual
+  ) where
+import qualified AlgebraicPrelude               as AP
+import           Data.Sized.Builtin             (Sized)
+import qualified Data.Sized.Builtin             as SV
+import           Data.Vector                    (Vector)
+import           Numeric.Algebra                (Additive, Algebra, (.*))
+import qualified Numeric.Algebra                as NA
 import           Numeric.Algebra.Smooth.Classes
 import           Numeric.Algebra.Smooth.Dual
-import           Numeric.Algebra.Smooth.Types
-import           Numeric.Natural
 
 diff1
   :: Floating b
